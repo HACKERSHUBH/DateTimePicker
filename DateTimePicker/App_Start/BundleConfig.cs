0becad9a49efe2b1a37d23efe048b88d4b77c8f2
Sharma <Shubham_Sharma12@Dell.com>
2019-04-13 18:59:00 +0530

    adding files

---
+++ DateTimePicker/App_Start/BundleConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace DateTimePicker.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+        //    bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+        //"~/Scripts/jquery-{version}.js"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //  "~/Scripts/bootstrap.js"));
+
+                bundles.Add(new StyleBundle("~/Content/css").Include(
+                           "~/Content/bootstrap.css",
+                          "~/Content/bootstrap-datetimepicker.css",
+                          "~/Content/Site.css"
+                          ));
+
+            bundles.Add(new ScriptBundle("~/bundles/myscripts").Include(
+                   "~/Scripts/jquery-3.3.1.js",
+                   "~/Scripts/moment.js",
+                   "~/Scripts/bootstrap.js",
+                   "~/Scripts/bootstrap-datetimepicker.js",
+                   "~/Scripts/Site.js"
+                   ));
+        }
+    }
+}
---
+++ DateTimePicker/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DateTimePicker.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        [Display(Name = "Full Name:")]
+        public string FullName { get; set; }
+        [Display(Name = "Email:")]
+        public string Email { get; set; }
+        [Display(Name = "Date of Birth:")]
+        public DateTime DateOfBirth { get; set; }
+        [Display(Name = "Time:")]
+        public string StartTime { get; set; }
+        [Display(Name = "Is Enrolled:")]
+        public bool IsEnrolled { get; set; }
+
+    }
+}
